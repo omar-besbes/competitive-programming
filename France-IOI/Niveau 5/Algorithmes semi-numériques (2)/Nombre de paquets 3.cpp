59a217551c6dd27d682896490e1cadf23c3ac3d9
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2024-03-28 15:07:14 +0100

    new: France-IOI niveau 5 algorithmes semi-numériques 2

---
+++ France-IOI/Niveau 5/Algorithmes semi-numériques (2)/Nombre de paquets 3.cpp
@@ -0,0 +1,23 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    int n, p;
+    cin >> n >> p;
+
+    if (p == 1)
+        cout << n << "\n";
+    else
+    {
+        vector<int> suff(n + 1, 1);
+        suff[n] = 0;
+        while (p--)
+        {
+            for (int i = n - 1; i >= 0; i--)
+                suff[i] += suff[i + 1];
+        }
+
+        cout << suff[0] << "\n";
+    }
+}
---
+++ France-IOI/Niveau 5/Algorithmes semi-numériques (2)/Réussite 2.cpp
@@ -0,0 +1,25 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> fact;
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0)
+        {
+            if (i != n / i)
+                cout << i << "\n";
+            fact.push_back(i);
+        }
+
+    if (fact.size())
+        for (int i = fact.size() - 1; i >= 0; i--)
+        {
+            cout << n / fact[i] << "\n";
+        }
+    else
+        cout << "0\n";
+}
