165f44da423baff21816d2473c0a261eaaaabf2c
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2024-07-22 01:56:14 +0100

    feat: stress tester

---
+++ stress-tester/optimal.cpp
@@ -0,0 +1,18 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+void solve()
+{
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
