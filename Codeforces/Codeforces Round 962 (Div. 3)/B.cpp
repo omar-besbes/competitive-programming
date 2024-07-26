0d43422896a82be775aec7fa1ba78181f1073e33
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2024-07-26 20:22:02 +0100

    new: Codeforces Round 962 (Div. 3)

---
+++ Codeforces/Codeforces Round 962 (Div. 3)/B.cpp
@@ -0,0 +1,31 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+        if (i % k == 0)
+        {
+            for (int j = 0; j < n; j += k)
+                cout << s[j] - '0';
+            cout << "\n";
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
