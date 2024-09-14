39859e95615453f6a933e47dc60d2bc17b2392bb
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2024-09-14 17:47:00 +0100

    new: Codeforces Round 972 (Div. 2)

---
+++ solutions/Codeforces/Codeforces Round 972 (Div. 2)/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    vector<char> v{'a', 'e', 'i', 'o', 'u'};
+    int base = n/5, r = n%5;
+    for(int i=0; i<5; i++) {
+        for(int j=0; j<base + (i < r ? 1 : 0); j++)
+            cout << v[i];
+    }
+    cout << "\n";
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+}
