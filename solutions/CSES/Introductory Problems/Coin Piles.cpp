19abe1321b83a0ec273d40b9dc275b63f89c4ff5
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2024-09-22 09:56:58 +0100

    new(CSES): Introductory Problems

---
+++ solutions/CSES/Introductory Problems/Coin Piles.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+
+    // auto op1 = ((a + b) / 3 + (a - b)) / 2;
+    // auto op2 = ((a + b) / 3 - (a - b)) / 2;
+
+    if ((a + b) % 3 == 0 && a <= 2 * b && b <= 2 * a)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
---
+++ solutions/CSES/Introductory Problems/Gray Code.cpp
@@ -0,0 +1,10 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < (1 << n); i++)
+        cout << (bitset<16>(i ^ (i >> 1))).to_string().substr(16 - n) << "\n";
+}
