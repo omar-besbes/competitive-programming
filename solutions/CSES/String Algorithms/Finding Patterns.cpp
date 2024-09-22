1c07d24064f1a5ef304c0456d2b3ec1b696e6f80
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2024-09-22 09:55:36 +0100

    new: CSES String Algorithms

---
+++ solutions/CSES/String Algorithms/Finding Patterns.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+int n, k;
+
+struct sa
+{
+    struct state
+    {
+        int link, len;
+        map<char, int> next;
+
+        state(int _len = 0, int _link = -1, map<char, int> _next = map<char, int>())
+        {
+            len = _len, link = _link, next = _next;
+        }
+    };
+
+    vector<state> st;
+    int last;
+
+    sa()
+    {
+        st.emplace_back();
+        last = 0;
+    }
+
+    bool find(string t)
+    {
+        for (int i = 0, cur = 0; i < t.size(); i++)
+        {
+            if (st[cur].next.count(t[i]))
+                cur = st[cur].next[t[i]];
+            else
+                return false;
+        }
+        return true;
+    }
+
+    void extend(char c)
+    {
+        int cur = st.size();
+        st.push_back(state(st[last].len + 1));
+
+        int p = last;
+        while (p != -1 && !st[p].next.count(c))
+        {
+            st[p].next[c] = cur;
+            p = st[p].link;
+        }
+
+        if (p == -1)
+        {
+            st[cur].link = 0;
+        }
+        else
+        {
+            int q = st[p].next[c];
+            if (st[q].len == st[q].len + 1)
+            {
+                st[cur].link = q;
+            }
+            else
+            {
+                int clone = st.size();
+                st.emplace_back(st[cur].len + 1, st[q].link, st[q].next);
+                while (p != -1 && st[p].next[c] == q)
+                {
+                    st[p].next[c] = clone;
+                    p = st[p].link;
+                }
+                st[q].link = st[cur].link = clone;
+            }
+        }
+
+        last = cur;
+    }
+};
+
+sa preprocess()
+{
+    sa ds;
+    for (int i = 0; i < n; i++)
+        ds.extend(s[i]);
+    return ds;
+}
+
+void solve()
+{
+    cin >> s;
+    n = s.length();
+    cin >> k;
+
+    auto ds = preprocess();
+
+    for (int i = 0; i < k; i++)
+    {
+        string t;
+        cin >> t;
+        cout << (ds.find(t) ? "YES\n" : "NO\n");
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+}
---
+++ solutions/CSES/String Algorithms/String Matching.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const int mod = 1e9 + 7;
+const int k = 3;
+
+vector<vector<ll>> p{{1, 31}, {1, 37}, {1, 41}};
+
+void compute_powers(int n)
+{
+    for (int j = 0; j < k; j++)
+    {
+        p[j].resize(n + 1);
+        for (int i = 2; i < n; i++)
+            p[j][i] = p[j][i - 1] * p[j][1] % mod;
+    }
+}
+
+vector<ll> hash1(string x, vector<ll> &p)
+{
+    int n = x.length();
+    vector<ll> h(n + 1);
+    for (int i = 0; i < n; i++)
+        h[i + 1] = (h[i] + (x[i] - 'a' + 1) * p[i]) % mod;
+    return h;
+}
+
+ll hash2(string x, vector<ll> &p)
+{
+    int n = x.length();
+    ll h = 0;
+    for (int i = 0; i < n; i++)
+        h = (h + (x[i] - 'a' + 1) * p[i]) % mod;
+    return h;
+}
+
+int res(string s, string t)
+{
+    int n = s.length(), m = t.length();
+
+    if (m > n)
+        return 0;
+
+    compute_powers(n);
+
+    vector<ll> ht(k, 0);
+    vector<vector<ll>> hs(k, vector<ll>(n + 1));
+    for (int i = 0; i < k; i++)
+        ht[i] = hash2(t, p[i]), hs[i] = hash1(s, p[i]);
+
+    int ans = 0;
+    for (int i = 0; i + m - 1 < n; i++)
+    {
+        bool yes = true;
+        for (int j = 0; j < k; j++)
+        {
+            ll cur = (hs[j][i + m] + mod - hs[j][i]) % mod;
+            if (cur != ht[j] * p[j][i] % mod)
+                yes = false;
+        }
+        if (yes)
+            ans++;
+    }
+
+    return ans;
+}
+
+void solve()
+{
+    string s, t;
+    cin >> s >> t;
+    cout << res(s, t) << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+}
