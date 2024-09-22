5dab4bcaa9f053a1edbee59b296cc3b8a89427d2
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2024-09-22 09:53:58 +0100

    new Codeforces Round 974 (Div. 3)

---
+++ solutions/Codeforces/Codeforces Round 974 (Div. 3)/B.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    int odd = k / 2 + (((n - k + 1) & 1) && (k & 1) ? 1 : 0);
+
+    cout << (odd & 1 ? "NO\n" : "YES\n");
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+}
