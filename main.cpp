78b6a5532987fc3d3857a428ac70b1cc887a348c
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2024-10-29 19:44:58 +0100

    chore: update cpp template

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define input "input.in"
+#define output "output.out"
 
 void solve() {}
 
@@ -7,6 +9,9 @@ int main() {
    ios::sync_with_stdio(false);
    cin.tie(0);
    cout.tie(0);
+
+   if (fopen(input, "r")) freopen(input, "r", stdin);
+   if (fopen(output, "r")) freopen(output, "w+", stdout);
 
    int t = 1;
    cin >> t;
